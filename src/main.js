ee5bfad2d1a1753f5b6d668fa77e337ab35fb5d7
Benjamin G. West <23172276+bgwest@users.noreply.github.com>
2018-10-15 19:31:29 -0700

    Code: Frontend Tooling and React

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import React from 'react'; // ES6
+import ReactDom from 'react-dom';
+import App from './components/app/app';
+import '../style/main.scss';
+
+// !: = development note:
+
+//------------------------------------------------------------
+
+const rootNode = document.createElement('div');
+document.body.appendChild(rootNode);
+// !: this will be what starts the entire application
+ReactDom.render(<App />, rootNode);
